6a3ab38fd1f1d35f48b821e19d644817b1434e1f
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-04-11 21:49:04 +0530

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">Hello</h1>
+    </>
+  );
+}
+
+export default App;
