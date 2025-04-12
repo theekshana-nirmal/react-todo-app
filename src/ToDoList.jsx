c6e1197853e1e39194919f60e3ea557956fbb981
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-04-12 21:22:00 +0530

    refactor: update ToDoList component with input handling and display tasks; remove Tailwind CSS imports

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 
-function ToDoList(){
-  const [tasks, setTasks] = useState(["Drink water", "Write a book", "Watch TV"])
-  const [newTask, setNewTask] = useState("")
+function ToDoList() {
+  const [tasks, setTasks] = useState([
+    "Drink water",
+    "Write a book",
+    "Watch TV",
+  ]);
+  const [newTask, setNewTask] = useState("");
 
-  function handleInputChange(event) {}
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
 
-  function handleAddTask(){}
+  function handleAddTask() {
+    setNewTask();
+  }
 
   function handleDeleteTask(index) {}
 
@@ -14,11 +22,19 @@ function ToDoList(){
 
   function handleMoveDown(index) {}
 
-  return(
-    <div>
-      <h1>To-Do List</h1>
-      <input type="text" />
-    </div>
+  return (
+    <>
+      <div>
+        <h1>To-Do List</h1>
+        <input type="text" onChange={handleInputChange} value={newTask} />
+        <button className="add-btn" onClick={handleAddTask}>
+          Add
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (<li key={index}><span className="text">{task}</span></li>))}
+      </ol>
+    </>
   );
 }
 
